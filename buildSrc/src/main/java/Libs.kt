9f1d2fd1a7d4ce96f6235cd3c33d19096b99d86d
Rony <rony.c2015@gmail.com>
2021-10-27 20:40:38 -0500

    Migrate to gradle.kts

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,6 @@
+object Libs {
+
+    const val koinAndroid = "io.insert-koin:koin-android:${Versions.koin}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,6 @@
+object Versions {
+
+
+    const val koin = "3.1.3"
+    const val retrofit = "2.9.0"
+}
