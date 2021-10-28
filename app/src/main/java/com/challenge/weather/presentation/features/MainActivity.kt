4e65b9e2f0d4243145fd6254b98ebea2bb173e55
Rony <rony.c2015@gmail.com>
2021-10-27 19:42:05 -0500

    initial setup

---
+++ app/src/main/java/com/challenge/weather/presentation/features/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.challenge.weather.presentation.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.challenge.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
