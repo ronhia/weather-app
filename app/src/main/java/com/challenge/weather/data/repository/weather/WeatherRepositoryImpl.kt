2e897fa54e0bcbca9c12bddc3d114765d45d8ce0
Rony <rony.c2015@gmail.com>
2021-10-29 15:33:22 -0500

    Clean

--- app/src/main/java/com/challenge/weather/data/repository/weather/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/challenge/weather/data/repository/weather/WeatherRepositoryImpl.kt
@@ -1,17 +1,12 @@
 package com.challenge.weather.data.repository.weather
 
 import com.challenge.weather.data.mapper.toDomain
-import com.challenge.weather.domain.model.Coordinates
 import com.challenge.weather.domain.model.Weather
 import com.challenge.weather.domain.repository.WeatherRepository
 
 class WeatherRepositoryImpl(
     private val weatherRemoteStorage: WeatherRemoteStorage
 ) : WeatherRepository {
-
-    override fun getByCoordinates(coordinates: Coordinates) {
-        TODO("Not yet implemented")
-    }
 
     override suspend fun getByNameCity(name: String): Weather {
         return weatherRemoteStorage.getByCityName(name).toDomain()
