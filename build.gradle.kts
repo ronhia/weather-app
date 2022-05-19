f5329448e60572e1cf4554daedcf78d72189c7c7
Rony <rony.c2015@gmail.com>
2022-05-19 15:34:10 -0500

    Add configuration for coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("com.android.application") version "7.1.1" apply false
     id("com.android.library") version "7.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
+    id("org.sonarqube") version "3.3"
 }
 
 buildscript {
@@ -11,6 +13,32 @@ buildscript {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.0")
     }
 }
+
+apply(plugin = "org.sonarqube")
+apply(plugin = "kover")
+
+tasks.koverMergedXmlReport {
+    enabled = true
+
+    includes = listOf("com.challenge.weather.*")
+}
+
+subprojects {
+    sonarqube {
+        properties {
+            property("sonar.sourceEncoding", "UTF-8")
+            property("sonar.sources", "$projectDir/src/main/java/")
+            property("sonar.tests", "$projectDir/src/test/java/")
+            property("sonar.exclusions", "**/di/*")
+            property(
+                "sonar.coverage.jacoco.xmlReportPaths",
+                "$buildDir/reports/kover/project-xml/report.xml"
+            )
+        }
+    }
+}
+
+project.tasks["sonarqube"].dependsOn("koverMergedXmlReport")
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
